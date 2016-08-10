226b740d0e26c898a656239756cb303d6ffa339f
Edward Barber <luckyshot423@gmail.com>
2016-08-10 18:49:52 -0400

    Updated

--- Safe Cracker/main.c
+++ Safe Cracker/main.c
@@ -11,18 +11,13 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-void safeNum(void);                        //askForNumber
-bool safeGues(int guess);                  //isValidGuess
+void safeNum(void);
+bool safeGues(int guess);
 
 int main(int argc, const char * argv[]) {
     
     printf("You are in a room with no doors and no windows.\nThere is only a safe, the way out is inside.\nCrack the combination and be freed. \n\n\n");
-    fflush(stdout);
-    sleep(3);
     printf("The Safe has a keypad with numbers 0-9. You only need four numbers to get the safe to open. Good luck... \n\n\n");
-    fflush(stdout);
-    sleep(3);
-    printf("Please enter a number between 0 and 9\n\n: ");
     
     
     int safeCombo[4] = {
@@ -35,27 +30,36 @@ int main(int argc, const char * argv[]) {
     int keyEntr = -1;
     int a = -1;
     
-    
-safeNum();
-    while (!safeGues(a)) {
+safeNum();                                                           //Error with my loop
+    while (!safeGues(a)) {                                           //
         fpurge(stdin);
         keyEntr = scanf("%d", &a);
-        
-        if (keyEntr != 1 || !safeGues(a)) {
+        if (keyEntr != 1 && !safeGues(a)) {
             printf("Please enter a number between 0 and 9\n\n: ");
             a = -1;
         }
     }
    
     
-    if(safeCombo == a) {
-        printf("Correct! The safe opens, a knife is found inside. \n\n");
-    } else {
-        printf("BEEP! The correct combo was %d, %d, %d, %d, \n", safeCombo);
+    if(safeCombo[0] == a) {
+        printf("First number is Correct!  \n\n");
     }
+    if(safeCombo[1] == a) {
+        printf("Second number is Correct!  \n\n");
+    }
+    if(safeCombo[2] == a) {
+        printf("Third number is Correct!  \n\n");
+    }
+    if(safeCombo[3] == a) {
+        printf("Correct! The safe opens, a knife is found inside.  \n\n");
+    }
+    else {
+        printf("BEEP! The correct combo was %d, %d, %d, %d, \n", safeCombo[0], safeCombo[1], safeCombo[2], safeCombo[3]);
+    }
+    return 0;
 }
 void safeNum() {
-    printf("Please enter a number. (From 0 to 9): ");
+    printf("Please enter a number. (From 0 to 9): "); //Why does this pop up before I've entered the safeCombinations? Because it pulls from safeNum on line 34?
 }
 
 bool safeGues(int guess) {
