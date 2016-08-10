30a50d625ef6efd4cbd29c165f3162b2c11ea1fa
Edward Barber <luckyshot423@gmail.com>
2016-08-10 15:06:41 -0400

    Initial Commit

---
+++ Safe Cracker/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Safe Cracker
+//
+//  Created by Andrew Barber on 8/10/16.
+//  Copyright © 2016 Invictus. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
