0164f05b1ae456d79b30d5c80ec0c99085a7cc7d
Edward Barber <luckyshot423@gmail.com>
2016-08-10 21:45:59 -0400

    Saving

--- Safe Cracker/main.c
+++ Safe Cracker/main.c
@@ -17,10 +17,10 @@ bool safeGues(int guess);
 int main(int argc, const char * argv[]) {
    
     
-
+    
+    
     printf("You are in a room with no doors and no windows.\nThere is only a safe, the way out is inside.\nCrack the combination and be freed. \n\n\n");
     printf("The Safe has a keypad with numbers 0-9. You only need four numbers to get the safe to open. Attempt to get the right comnination. Good luck... \n\n\n");
-    
     
     int safeCombo[4] = {
         arc4random_uniform(9),
@@ -68,7 +68,6 @@ int main(int argc, const char * argv[]) {
                 d = -1;
         }
     }
-   
     
     if(safeCombo[0] == a) {
         printf("First number is Correct!  \n\n");
@@ -83,12 +82,13 @@ int main(int argc, const char * argv[]) {
         printf("Correct! The safe opens, a knife is found inside.  \n\n");
     }
     else {
-        printf("BEEP! The correct combo was %d, %d, %d, %d, \n", safeCombo[0], safeCombo[1], safeCombo[2], safeCombo[3]);
+        printf("BEEP! The correct combo was %d, %d, %d, %d, \n\n", safeCombo[0], safeCombo[1], safeCombo[2], safeCombo[3]);
+        printf("To play again press -> in the top left hand corner!\n\n");
     }
     return 0;
 }
     void safeNum() {
-        printf("Please enter a number. (From 0 to 9): "); //Why does this pop up before I've entered the safeCombinations? Because it pulls from safeNum on line 34?
+        printf("Please enter a number. (From 0 to 9): ");
     }
     
     bool safeGues(int guess) {
